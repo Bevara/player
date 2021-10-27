21ccace9fd5f1f0792f232dc7e580d12dc221f25
jgn <gorinje@gmail.com>
2021-10-27 20:34:29 +0200

    Start loading nanojpeg

--- webinterface/UImage.ts
+++ webinterface/UImage.ts
@@ -1,12 +1,53 @@
+import {asmLibraryArg, initRuntime, stackCheckInit, Module, wasmTable, wasmMemory, stringToUTF8, stackAlloc} from './player'
+import { GOTHandler } from "./imports";
+
+const info: WebAssembly.Imports = {
+    'env': asmLibraryArg,
+    'wasi_snapshot_preview1': asmLibraryArg,
+    'GOT.mem': new Proxy(asmLibraryArg, GOTHandler),
+    'GOT.func': new Proxy(asmLibraryArg, GOTHandler)
+};
+//info.env["__indirect_function_table"] = self.wasmTable;
+//info.env["memory"] = self.wasmMemory;
+
+info.env["__indirect_function_table"] = wasmTable;
+info.env["memory"] = wasmMemory;
+
 class UniversalImage extends HTMLImageElement {
+    using : string;
 
     connectedCallback() {
         let self = this;
 
-        fetch(self.src).then(async function (response) {
-            const img = await response.blob();
-            self.srcset = URL.createObjectURL(img);
-        });
+        let downloads : {[key: string]: Promise<Response>} = {};
+
+        function flushImage(responses : Response[]) : void {
+            const img_response = responses[promises.indexOf(downloads["src"])];
+            const using_response = responses[promises.indexOf(downloads["using"])];
+
+            let blobs : [Promise<Blob>, Promise<WebAssembly.WebAssemblyInstantiatedSource>] = [img_response.blob(), WebAssembly.instantiateStreaming(using_response, info)];
+            Promise.all(blobs).then((result) => {
+                const img_blob = result[0];
+                const using_wasm = result[1];
+                const exports = using_wasm.instance.exports;
+
+                self.srcset = URL.createObjectURL(img_blob); 
+            });
+        }
+
+        for (var i = 0, atts = self.attributes, n = atts.length, arr = []; i < n; i++) {
+            const nodeName = atts[i].nodeName;
+            switch(nodeName){
+                case 'using':
+                case 'src':
+                    downloads[nodeName] = fetch(atts[i].nodeValue);
+                    break;
+            }
+        }
+
+        let promises : Promise<Response>[] = Object.values(downloads);
+
+        Promise.all(promises).then(flushImage);
     }
 
     disconnectedCallback() {
