d648b8bb7211a5fa97d09c903c9e9671e2d6ea4e
jgn <gorinje@gmail.com>
2021-12-27 18:16:38 +0100

    Add non regression test for development

--- test/test.js
+++ test/test.js
@@ -1,7 +1,32 @@
-describe('Array', () => {
-    describe('#indexOf()', () => {
-      it('should return -1 when the value is not present', () => {
-        assert.equal(-1, [1,2,3].indexOf(4));
-      });
+
+
+describe('Simple-img', () => {
+  describe('#nanojpeg', () => {
+    it('should decode Freedom.jpeg', (done) => {
+      new Promise(function (resolve) {
+        const img = document.createElement('img', { "is": "universal-img" });
+        img.setAttribute("src", "http://bevaraserver.ddns.net/test-signals/Freedom.jpg");
+        img.setAttribute("using", "simple-img.wasm");
+        img.setAttribute("with", "nanojpeg.wasm");
+        document.body.appendChild(img);
+        const fetchs = img.decodingPromise.then(src =>
+          Promise.all([
+            fetch(src),
+            fetch("http://bevaraserver.ddns.net/test-signals/Freedom.png")
+          ]));
+
+        fetchs.then((responses) => {
+          arrayBuffers = responses.map(response => response.arrayBuffer());
+          Promise.all(arrayBuffers)
+            .then(buffers => {
+              const result = new Uint8Array(buffers[0]);
+              const ref = new Uint8Array(buffers[1]);
+              assert.deepEqual(result, ref);
+              resolve();
+            });
+        });
+      })
+        .then(done);
     });
   });
+});
--- webinterface/UImage.ts
+++ webinterface/UImage.ts
@@ -1,21 +1,27 @@
 //import {HEAPU8, asmLibraryArg, writeArrayToMemory, initRuntime, stackCheckInit, Module, wasmTable, wasmMemory, stringToUTF8, stackAlloc} from './player'
 import { Common } from "./common"
-import {Module, using_file} from "./simple-img.js"
+import { Module, using_file } from "./simple-img.js"
 
 //declare var Module: any;
 
 class UniversalImage extends HTMLImageElement {
     using: string;
     common: Common;
-    memory :Uint8Array;
+    memory: Uint8Array;
 
-    with:string;
+    with: string;
 
-    instance:any;
-    entry:any;
-    module:any;
+    instance: any;
+    entry: any;
+    module: any;
 
-    static flush_image(entry: number) {
+    private _decodingPromise: Promise<String>;
+
+    get decodingPromise() {
+        return this._decodingPromise;
+    }
+
+    static flush_image(entry: number): void {
         let self = this as any;
 
         const get_args = JSON.stringify([
@@ -25,14 +31,14 @@ class UniversalImage extends HTMLImageElement {
         const ptr_get_args = self.instance.stackAlloc(get_args_len);
 
         self.instance.stringToUTF8(get_args, ptr_get_args, get_args_len);
-        
+
         const ptr_data = self.instance._get(self.entry, ptr_get_args);
 
         const json = self.instance.UTF8ToString(ptr_data);
         const json_parsed = JSON.parse(json);
 
-        if (json_parsed.getImage == 0 ) return;
-        
+        if (json_parsed.getImage == 0) return;
+
         const image = self.instance.HEAPU8.slice(json_parsed.getImage, json_parsed.getImage + json_parsed.getSize);
 
         let canvas = document.createElement('canvas');
@@ -60,59 +66,14 @@ class UniversalImage extends HTMLImageElement {
             self.srcset = URL.createObjectURL(blob);
         })
     }
-    
+
 
     connectedCallback() {
         const self = this;
         const downloads: { [key: string]: Promise<Response> } = {};
         const using_attribute = self.getAttribute("using");
         const with_attribute = self.getAttribute("with");
-        let args : any = {};
-
-        function decode(responses: Response[]): void{
-            const img_response = responses[promises.indexOf(downloads["src"])];
-            const module = responses[promises.indexOf(downloads["module"])];
-            self.module = module;
-
-            let blobs: [Promise<ArrayBuffer>] = [img_response.arrayBuffer()];
-
-            Promise.all(blobs).then( (result) => {
-                const img_array = result[0];
-                self.entry = self.module._constructor();
-
-                const ptr = self.module.stackAlloc(img_array.byteLength);
-                self.module.HEAPU8.set(new Uint8Array(img_array), ptr);
-                args["buffer"] = {pointer : ptr, size : img_array.byteLength};
-                const json_args = JSON.stringify(args);
-
-                const len_args = (json_args.length << 2) + 1;
-                const ptr_args = self.module.stackAlloc(len_args);
-                self.module.stringToUTF8(json_args, ptr_args, len_args);
-
-                self.module._set(self.entry, ptr_args);
-
-                const get_args = JSON.stringify([
-                    "getImage", "getSize", "getWidth", "getHeight"
-                ]);
-
-                const get_args_len = (get_args.length << 2) + 1;
-                const ptr_get_args = self.module.stackAlloc(get_args_len);
-
-                self.module.stringToUTF8(get_args, ptr_get_args, get_args_len);
-
-                const ptr_data = self.module._get(self.entry, ptr_get_args);
-                const json_res = self.module.UTF8ToString(ptr_data);
-                const json_res_parsed = JSON.parse(json_res);
-
-                const image = self.module.HEAPU8.slice(json_res_parsed.getImage, json_res_parsed.getImage + json_res_parsed.getSize);
-                const blob = new Blob([image]);
-                self.srcset = URL.createObjectURL(blob);
-
-            });
-        }
-
-
-        
+        let args: any = {};
 
         for (var i = 0, atts = this.attributes, n = atts.length, arr = []; i < n; i++) {
             const nodeName = atts[i].nodeName;
@@ -121,22 +82,72 @@ class UniversalImage extends HTMLImageElement {
                     downloads[nodeName] = fetch(atts[i].nodeValue);
 
                 default:
-                args[nodeName] = atts[i].nodeValue;
+                    args[nodeName] = atts[i].nodeValue;
             }
         }
 
-        const _scriptDir = (document.currentScript as any).src;
-        const scriptDirectory = _scriptDir.substr(0, _scriptDir.lastIndexOf('/')+1)
+        var _scriptDir;
 
+        if (document.currentScript) {
+            _scriptDir = (document.currentScript as any).src;
+        } else {
+            _scriptDir = "/base/build/dist/"; //FIXME : for testing purpose
+        }
+
+        const scriptDirectory = _scriptDir.substr(0, _scriptDir.lastIndexOf('/') + 1)
+        
         using_file.location = using_attribute;
-
         downloads["module"] = new (Module as any)({
-            dynamicLibraries: [scriptDirectory+with_attribute]
-            });
+            dynamicLibraries: [scriptDirectory + with_attribute]
+        });
 
-        let promises: Promise<Response>[] = Object.values(downloads);
+        this._decodingPromise = new Promise((main_resolve, main_reject) => {
+            function decode(responses: Response[]): void {
+                const img_response = responses[promises.indexOf(downloads["src"])];
+                const module = responses[promises.indexOf(downloads["module"])];
+                self.module = module;
+    
+                let blobs: [Promise<ArrayBuffer>] = [img_response.arrayBuffer()];
+    
+                Promise.all(blobs).then((result) => {
+                    const img_array = result[0];
+                    self.entry = self.module._constructor();
+    
+                    const ptr = self.module.stackAlloc(img_array.byteLength);
+                    self.module.HEAPU8.set(new Uint8Array(img_array), ptr);
+                    args["buffer"] = { pointer: ptr, size: img_array.byteLength };
+                    const json_args = JSON.stringify(args);
+    
+                    const len_args = (json_args.length << 2) + 1;
+                    const ptr_args = self.module.stackAlloc(len_args);
+                    self.module.stringToUTF8(json_args, ptr_args, len_args);
+    
+                    self.module._set(self.entry, ptr_args);
+    
+                    const get_args = JSON.stringify([
+                        "getImage", "getSize", "getWidth", "getHeight"
+                    ]);
+    
+                    const get_args_len = (get_args.length << 2) + 1;
+                    const ptr_get_args = self.module.stackAlloc(get_args_len);
+    
+                    self.module.stringToUTF8(get_args, ptr_get_args, get_args_len);
+    
+                    const ptr_data = self.module._get(self.entry, ptr_get_args);
+                    const json_res = self.module.UTF8ToString(ptr_data);
+                    const json_res_parsed = JSON.parse(json_res);
+    
+                    const image = self.module.HEAPU8.slice(json_res_parsed.getImage, json_res_parsed.getImage + json_res_parsed.getSize);
+                    const blob = new Blob([image]);
+                    self.srcset = URL.createObjectURL(blob);
+                    main_resolve(self.srcset);
+                });
+            }
 
-        Promise.all(promises).then(decode);
+            let promises: Promise<Response>[] = Object.values(downloads);
+
+            Promise.all(promises).then(decode);
+        });
     }
 
     disconnectedCallback() {
