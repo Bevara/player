cbef3e9107a3edfcf56b586252ed55e5e475d492
jgn <gorinje@gmail.com>
2022-01-12 21:16:45 +0100

    Fix both jpeg and j2k test

--- test/test.js
+++ test/test.js
@@ -29,4 +29,33 @@ describe('Simple-img', () => {
         .then(done);
     });
   });
+
+  describe('#j2kdec', () => {
+    it('should decode Cevennes2.jp2', (done) => {
+      new Promise(function (resolve) {
+        const img = document.createElement('img', { "is": "universal-img" });
+        img.setAttribute("src", "http://bevaraserver.ddns.net/test-signals/j2k/Cevennes2.jp2");
+        img.setAttribute("using", "simple-img.wasm");
+        img.setAttribute("with", "j2kdec.wasm");
+        document.body.appendChild(img);
+        const fetchs = img.decodingPromise.then(src =>
+          Promise.all([
+            fetch(src),
+            fetch("http://bevaraserver.ddns.net/test-signals/out/j2k/Cevennes2.png")
+          ]));
+
+        fetchs.then((responses) => {
+          arrayBuffers = responses.map(response => response.arrayBuffer());
+          Promise.all(arrayBuffers)
+            .then(buffers => {
+              const result = new Uint8Array(buffers[0]);
+              const ref = new Uint8Array(buffers[1]);
+              expect(result).to.eql(ref);
+              resolve();
+            });
+        });
+      })
+        .then(done);
+    });
+  });
 });
