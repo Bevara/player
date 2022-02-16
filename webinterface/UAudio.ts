ba3e513981a7f18ac1a0d12157dfd66d73b45ece
jgn <gorinje@gmail.com>
2022-02-16 19:48:56 +0100

    Update memory management

--- webinterface/UAudio.ts
+++ webinterface/UAudio.ts
@@ -1,9 +1,7 @@
 //import {HEAPU8, asmLibraryArg, writeArrayToMemory, initRuntime, stackCheckInit, Module, wasmTable, wasmMemory, stringToUTF8, stackAlloc} from './player'
 import { Common } from "./common"
 import { Module, location } from "./simple-img.js"
-import {fileio} from "./memio"
-
-//declare var Module: any;
+import { fileio } from "./memio"
 
 class UniversalAudio extends HTMLAudioElement {
     using: string;
@@ -15,8 +13,7 @@ class UniversalAudio extends HTMLAudioElement {
     instance: any;
     entry: any;
     module: any;
-    input : fileio;
-    output : fileio;
+    io: fileio;
 
     private _decodingPromise: Promise<String>;
 
@@ -26,96 +23,77 @@ class UniversalAudio extends HTMLAudioElement {
 
     connectedCallback() {
         const self = this;
-        const downloads: { [key: string]: Promise<Response> } = {};
         const using_attribute = self.getAttribute("using");
         const with_attribute = self.getAttribute("with");
         let args: any = {};
 
         for (var i = 0, atts = this.attributes, n = atts.length, arr = []; i < n; i++) {
             const nodeName = atts[i].nodeName;
-            switch (nodeName) {
-                case 'src':
-                    downloads[nodeName] = fetch(atts[i].nodeValue);
-                    this.input = new fileio(atts[i].nodeValue);
-                default:
-                    args[nodeName] = atts[i].nodeValue;
-            }
+            args[nodeName] = atts[i].nodeValue;
         }
 
-        this.output = new fileio("out.wav");
         location.using = using_attribute;
         location.with = with_attribute;
-        downloads["module"] = new (Module as any)({
-            dynamicLibraries: [with_attribute,
-                "rfflac.wasm",
-                "writegen.wasm"
-            ]
-        });
+        this.io = new fileio();
 
         this._decodingPromise = new Promise((main_resolve, _main_reject) => {
-            function decode(responses: Response[]): void {
-                const img_response = responses[promises.indexOf(downloads["src"])];
-                const module = responses[promises.indexOf(downloads["module"])];
+            new (Module as any)({
+                dynamicLibraries: [with_attribute,
+                    "rfflac.wasm",
+                    "writegen.wasm"
+                ]
+            }).then(module => {
                 self.module = module;
+                self.io.module = module;
+                self.entry = self.module._constructor();
+                let buffer_in = self.io.make_fileio(self.src, true);
+                let buffer_out = self.io.make_fileio("out.wav", false);
+                args["io_in"] = buffer_in.file_io;
+                args["io_out"] = buffer_out.file_io;
 
-                let blobs: [Promise<ArrayBuffer>] = [img_response.arrayBuffer()];
+                // Set input filters
+                args["filters"] = self.module.filter_entries.map(entry => self.module["_" + entry]());
 
-                Promise.all(blobs).then((result) => {
-                    const img_array = result[0];
-                   
-                    self.entry = self.module._constructor();
-                    self.input.buffer = new Uint8Array(img_array);
-                    self.input.module = self.module;
-                    self.output.buffer = new Uint8Array(0);
-                    self.output.module = self.module;
+                Promise.all(self.io.fetch_promises).then(res_fetch => {
+                    Promise.all(self.io.buffer_promises).then(res_buffer => {
 
-                    args["io_in"] = self.input.make_fileio();
-                    args["io_out"] = self.output.make_fileio();
+                        // Convert json to string buffer
+                        const json_args = JSON.stringify(args);
+                        const len_args = (json_args.length << 2) + 1;
+                        const ptr_args = self.module.stackAlloc(len_args);
+                        self.module.stringToUTF8(json_args, ptr_args, len_args);
 
-                    // Set input filters
-                    args["filters"] = self.module.filter_entries.map(entry => self.module["_" + entry]());
+                        // Call set function and decode
+                        self.module._set(self.entry, ptr_args);
 
-                    // Convert json to string buffer
-                    const json_args = JSON.stringify(args);
-                    const len_args = (json_args.length << 2) + 1;
-                    const ptr_args = self.module.stackAlloc(len_args);
-                    self.module.stringToUTF8(json_args, ptr_args, len_args);
+                        // Retrieve result
+                        const props = [];
+                        if (self.hasAttribute("connections")) {
+                            props.push("connections");
+                        }
 
-                    // Call set function and decode
-                    self.module._set(self.entry, ptr_args);
+                        const get_args = JSON.stringify(props);
 
-                    // Retrieve result
-                    const props = [
-                    ]
-                    if (self.hasAttribute("connections")) {
-                        props.push("connections");
-                    }
+                        const get_args_len = (get_args.length << 2) + 1;
+                        const ptr_get_args = self.module.stackAlloc(get_args_len);
 
-                    const get_args = JSON.stringify(props);
+                        self.module.stringToUTF8(get_args, ptr_get_args, get_args_len);
 
-                    const get_args_len = (get_args.length << 2) + 1;
-                    const ptr_get_args = self.module.stackAlloc(get_args_len);
+                        const ptr_data = self.module._get(self.entry, ptr_get_args);
+                        const json_res = self.module.UTF8ToString(ptr_data);
+                        const json_res_parsed = JSON.parse(json_res);
 
-                    self.module.stringToUTF8(get_args, ptr_get_args, get_args_len);
-
-                    const ptr_data = self.module._get(self.entry, ptr_get_args);
-                    const json_res = self.module.UTF8ToString(ptr_data);
-                    const json_res_parsed = JSON.parse(json_res);
-
-                    const blob = new Blob([self.output.buffer], {type : "audio/wave"});
-                    var source = document.createElement('source');
-                    source.src = URL.createObjectURL(blob);
-                    source.type = "audio/wave";
-                    self.src = "";
-                    self.load();
-                    self.appendChild(source);
-                    main_resolve(source.src);
+                        const blob = new Blob([buffer_out.buffer_u8], {type : "audio/wave"});
+                        var source = document.createElement('source');
+                        source.src = URL.createObjectURL(blob);
+                        source.type = "audio/wave";
+                        self.src = "";
+                        self.load();
+                        self.appendChild(source);
+                        main_resolve(source.src);
+                    });
                 });
-            }
-
-            let promises: Promise<Response>[] = Object.values(downloads);
-
-            Promise.all(promises).then(decode);
+            });
         });
     }
 
--- webinterface/memio.ts
+++ webinterface/memio.ts
@@ -1,9 +1,44 @@
 import { memio } from "./simple-img.js"
+import { Fetch } from "./utils/fetch.js";
+
+
+class buffer {
+    buffer:ArrayBuffer;
+    buffer_u8:Uint8Array;
+    file_io:string;
+    p:number = 0;
+    nb_refs:number = 0;
+    src:string;
+    src_ptr:any;
+    write : Boolean;
+
+    constructor(src:string, src_ptr:any, write:Boolean) {
+        this.src = src;
+        this.src_ptr = src_ptr;
+        this.write = write;
+    }
+
+    open(mode:string){
+        switch(mode[0]){
+            case "r":
+                this.buffer_u8 = new Uint8Array(this.buffer);
+                break;
+
+            case "w":
+                this.buffer_u8 = new Uint8Array(0);
+                break;
+        }
+    }
+
+    size(){
+        return this.buffer_u8.length;
+    }
+}
 
 class fileio{
-    src:string;
-    buffer:Uint8Array;
-    p:number = 0;
+    fetch_promises:Promise<Response>[]= new Array();
+    buffer_promises:Promise<ArrayBuffer>[]= new Array();
+    io_ctxs:buffer[]= new Array();
     module:any;
     read:any;
     write:any;
@@ -16,57 +51,66 @@ class fileio{
     readonly SEEK_CUR = 1;
     readonly SEEK_END = 2;
 
-    constructor(src:string) {
-        this.src = src;
+
+    constructor() {
         this.createIO();
     }
 
     createIO(){
         this.read = function (fileio, buffer, bytes) {
-            console.log("reading from "+this.p+" to "+ (this.p+bytes));
-            let remaining = this.buffer.length - this.p;
+            const ioctx_id = this.module._gf_fileio_get_udta(fileio);
+            const mem = this.io_ctxs[ioctx_id];
+
+            //console.log("reading from "+mem.p+" to "+ (mem.p+bytes));
+            let remaining = mem.buffer_u8.length - mem.p;
         
             if (bytes > remaining) {
-                this.module.HEAPU8.set(this.buffer.slice(this.p, this.p + remaining), buffer);
-                this.p += remaining;
+                this.module.HEAPU8.set(mem.buffer_u8.slice(mem.p, mem.p + remaining), buffer);
+                mem.p += remaining;
                 return remaining;
             }
         
-            this.module.HEAPU8.set(this.buffer.slice(this.p, this.p + bytes), buffer);
-            this.p += bytes;
+            this.module.HEAPU8.set(mem.buffer_u8.slice(mem.p, mem.p + bytes), buffer);
+            mem.p += bytes;
             return bytes;
         }.bind(this);
 
         this.read.sig = ['i', 'i', 'i','i'];
 
         this.write = function (fileio, buffer, bytes) {
-            console.log("write from "+this.p+" to "+ (this.p+bytes));
-            let remaining = this.buffer.length - this.p;
+            const ioctx_id = this.module._gf_fileio_get_udta(fileio);
+            const mem = this.io_ctxs[ioctx_id];
+
+            //console.log("write from "+mem.p+" to "+ (mem.p+bytes));
+            let remaining = mem.buffer_u8.length - mem.p;
 
             if (bytes > remaining) {
-                let new_size = bytes + this.buffer.length - remaining;
-                let old_buffer = this.buffer;
-                this.buffer = new Uint8Array(new_size);
-                this.buffer.set(old_buffer);
+                let new_size = bytes + mem.buffer_u8.length - remaining;
+                let old_buffer = mem.buffer_u8;
+                mem.buffer_u8 = new Uint8Array(new_size);
+                mem.buffer_u8.set(old_buffer);
             }
             
-            this.buffer.set(this.module.HEAPU8.slice(buffer, buffer +bytes), this.p);
-            this.p += bytes;
+            mem.buffer_u8.set(this.module.HEAPU8.slice(buffer, buffer +bytes), mem.p);
+            mem.p += bytes;
             return bytes;
         }.bind(this);
         this.write.sig = ['i', 'i', 'i','i'];
 
         this.seek = function (fileio, offset, whence) {
-            console.log("seek from "+offset+" with "+ whence);
+            const ioctx_id = this.module._gf_fileio_get_udta(fileio);
+            const mem = this.io_ctxs[ioctx_id];
+
+            //console.log("seek from "+offset+" with "+ whence);
             switch (whence) {
                 case this.SEEK_SET:
-                    this.p = Number(offset);
+                    mem.p = Number(offset);
                     break;
                 case this.SEEK_CUR:
-                    this.p += Number(offset);
+                    mem.p += Number(offset);
                     break;
                 case this.SEEK_END:
-                    this.p = this.buffer.length + Number(offset);
+                    mem.p = mem.buffer_u8.length + Number(offset);
                     break;
             }
             return 0;
@@ -74,19 +118,25 @@ class fileio{
         this.seek.sig = ['i', 'i', 'j','i'];
 
         this.tell = function (fileio) {
-            console.log("tell  ");
-            return BigInt(this.p);
+            const ioctx_id = this.module._gf_fileio_get_udta(fileio);
+            const mem = this.io_ctxs[ioctx_id];
+
+            //console.log("tell : " + mem.p);
+            return BigInt(mem.p);
         }.bind(this);
         this.tell.sig = ['j', 'i'];
 
         this.eof = function (fileio) {
-            if(this.p == this.buffer.length){
+            const ioctx_id = this.module._gf_fileio_get_udta(fileio);
+            const mem = this.io_ctxs[ioctx_id];
+
+            /*if(mem.p == mem.buffer_u8.length){
                 console.log("eof  : true");
             }else{
                 console.log("eof  : false");
-            }
+            }*/
             
-            return this.p == this.buffer.length;
+            return mem.p == mem.buffer_u8.length;
         }.bind(this);
         this.eof.sig = ['i', 'i'];
 
@@ -97,22 +147,140 @@ class fileio{
         }
         this.printf.sig = ['i','i','i', 'i'];
         
-        this.open = function (fileio_ref, url, mode, out_err) {
-            console.log("open  ");
-            this.module._gf_fileio_set_stats_u32(fileio_ref, this.buffer.length,this.buffer.length, 1, 0);
+        this.open = function (fileio_ref, url_ptr, mode_ptr, out_err) {
+            //console.log("open  ");
+
+            const mode = this.module.UTF8ToString(mode_ptr);
+            const url = this.module.UTF8ToString(url_ptr);
+            const ioctx_id = this.module._gf_fileio_get_udta(fileio_ref);
+            const cur_buffer = this.io_ctxs[ioctx_id];
+
             this.module.HEAP32[((out_err)>>2)] = 0; //GF_OK
-            return fileio_ref;
+
+            if (mode == "ref") {
+                cur_buffer.nb_refs++;
+                return fileio_ref;
+            }
+
+            if (mode == "unref") {
+                if (!cur_buffer.nb_refs) return 0;
+                cur_buffer.nb_refs--;
+                if (cur_buffer.nb_refs)
+                    return fileio_ref;
+                this.module.HEAP8[url] = 0;
+            }
+
+            if (mode == "url") {
+                if (!url_ptr) return 0;
+                const path_ptr = this.module._gf_url_concatenate(cur_buffer.src_ptr, url_ptr);
+                const path = this.module.UTF8ToString(path_ptr);
+
+                let new_buffer = new buffer(path, path_ptr, cur_buffer.is_input);
+                let buffer_idx = this.io_ctxs.push(new_buffer);
+
+                const gfio = this.module._gf_fileio_new(path_ptr, buffer_idx - 1, 
+                    this.open.value, 
+                    this.seek.value, 
+                    this.read.value, 
+                    this.write.value, 
+                    this.tell.value,
+                    this.eof.value,
+                    this.printf.value);
+                
+                return gfio;
+            }
+
+            if (mode == "probe") {
+                if (!this.module._gf_file_exists(url_ptr)){
+                    this.module.HEAP32[((out_err)>>2)] = -12; //GF_URL_ERROR
+                }
+                return 0;
+            }
+
+            if (!url_ptr) {
+        
+                if (!cur_buffer.nb_refs) {
+                    this.io_ctxs.splice(ioctx_id,1);
+                }
+                return 0;
+            }
+
+            //file handle not opened, we can use the current gfio
+            if (!cur_buffer.buffer_u8 && url.startsWith("gfio://") || url == cur_buffer.src ) {
+                cur_buffer.open(mode);
+
+                //in test mode we want to use our ftell and fseek wrappers
+                if (mode[0] == 'r') {
+                    this.module._gf_fileio_set_stats_u32(fileio_ref, cur_buffer.size(),cur_buffer.size(), 1, 0);         
+                }
+                return fileio_ref;
+            }
+
+            let gfio= null;
+            let ioctx= null;
+            let ioctx_idx= 0;
+            let no_concatenate = false;
+            let new_buffer = null;
+            for (let i=0; i< this.io_ctxs.length; i++) {
+                ioctx = this.io_ctxs[i];
+                if (ioctx.src == url) {
+                    if (ioctx.buffer_u8) {
+                        no_concatenate = true;
+                        ioctx = null;
+                    }
+                    ioctx_idx = i;
+                    break;
+                }
+                ioctx = null;
+            }
+
+            if (!ioctx) {
+                let path_ptr = null;
+                if (url.startsWith("gfio://")) {
+                    if (no_concatenate){
+                        path_ptr = this.module._gf_strdup(url_ptr);
+                    }
+                    else{
+                        path_ptr = this.module._gf_url_concatenate(cur_buffer.src_ptr, url_ptr);
+                    }
+                        
+                } else {
+                    path_ptr = this.module.gf_strdup(cur_buffer.src_ptr);
+                }
+
+                new_buffer = new buffer(url, path_ptr, cur_buffer.is_input);
+                new_buffer.buffer = cur_buffer.buffer;
+                new_buffer.open(mode);
+                let buffer_idx = this.io_ctxs.push(new_buffer);
+                gfio = this.module._gf_fileio_new(path_ptr, buffer_idx - 1, 
+                    this.open.value, 
+                    this.seek.value, 
+                    this.read.value, 
+                    this.write.value, 
+                    this.tell.value,
+                    this.eof.value,
+                    this.printf.value);
+                if (mode[0] == 'r') {
+                    this.module._gf_fileio_set_stats_u32(fileio_ref, cur_buffer.size(),cur_buffer.size(), 1, 0);         
+                }
+            }
+
+            return gfio;
         }.bind(this);
         this.open.sig = ['i','i','i','i', 'i'];
         memio.push(this.read, this.write, this.open, this.seek, this.tell,this.eof,this.printf, this.open)
     }
 
-    make_fileio (){
-        const len_source_str = (this.src.length << 2) + 1;
+    make_fileio (src : string, is_input : Boolean){
+
+        const len_source_str = (src.length << 2) + 1;
         const ptr_source_str = this.module.stackAlloc(len_source_str);
-        this.module.stringToUTF8(this.src, ptr_source_str, len_source_str);
+        this.module.stringToUTF8(src, ptr_source_str, len_source_str);
+
+        let new_buffer = new buffer(src, ptr_source_str, is_input);
+        let buffer_idx = this.io_ctxs.push(new_buffer);
     
-        const fio = this.module._gf_fileio_new(ptr_source_str, 0, 
+        const fio = this.module._gf_fileio_new(ptr_source_str, buffer_idx - 1, 
             this.open.value, 
             this.seek.value, 
             this.read.value, 
@@ -122,7 +290,22 @@ class fileio{
             this.printf.value);
 
         const fio_url_ptr = this.module._gf_fileio_url(fio);
-        return this.module.UTF8ToString(fio_url_ptr);
+
+        if (is_input){
+            const fetch_promise = fetch(src);
+            this.fetch_promises.push(fetch_promise);
+            fetch_promise.then(response => {
+                const buffer_promise = response.arrayBuffer();
+                this.buffer_promises.push(buffer_promise);
+
+                buffer_promise.then( buffer =>{
+                    new_buffer.buffer = buffer;
+                });
+            })
+        }
+
+        new_buffer.file_io = this.module.UTF8ToString(fio_url_ptr);
+        return new_buffer;
     }
 }
 
