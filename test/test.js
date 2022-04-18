9a531f8033c4b230864fef32d2e75cd3f1f6a22c
jgn <gorinje@gmail.com>
2022-04-18 20:21:46 +0200

    New test for flac

--- test/test.js
+++ test/test.js
@@ -77,7 +77,7 @@ describe('core-player', () => {
       "http://bevara.ddns.net/test-signals/out/maddec/ImagineDragons.wav",
       done
       );
-    }).timeout(5000);
+    }).timeout(10000);
   });
 
   describe('#a52dec', () => {
@@ -93,16 +93,16 @@ describe('core-player', () => {
     }).timeout(10000);
   });
 
-  //   describe('#rfflac', () => {
-  //   it('should decode ff-16b-1c-44100hz.flac"', (done) => {
-  //     create_test('audio',
-  //     "universal-audio",
-  //     "core-player.wasm",
-  //     "rfflac.wasm;ffdec.wasm",
-  //     "http://bevara.ddns.net/test-signals/ff-16b-1c-44100hz.flac",
-  //     "http://bevaraserver.ddns.net/test-signals/out/flac/ff-16b-1c-44100hz.wav",
-  //     done
-  //     );
-  //   });
-  // }).timeout(5000);
+    describe('#rfflac', () => {
+    it('should decode ff-16b-1c-44100hz.flac"', (done) => {
+      create_test('audio',
+      "universal-audio",
+      "core-player.wasm",
+      "rfflac.wasm;ffdec.wasm",
+      "http://bevara.ddns.net/test-signals/ff-16b-1c-44100hz.flac",
+      "http://bevara.ddns.net/test-signals/out/flac/ff-16b-1c-44100hz.wav",
+      done
+      );
+    }).timeout(60000);
+  })
 });
