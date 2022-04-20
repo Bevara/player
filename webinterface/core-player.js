12719fb838596d98668d4b23106b6b62372f8c6d
jgn <gorinje@gmail.com>
2022-04-20 14:34:52 +0200

    Fix O3 compilation

--- webinterface/core-player.js
+++ webinterface/core-player.js
@@ -1771,9 +1771,9 @@ function checkStackCookie() {
   var max = _emscripten_stack_get_end();
   var cookie1 = HEAPU32[((max)>>2)];
   var cookie2 = HEAPU32[(((max)+(4))>>2)];
-  if (cookie1 != 0x2135467 || cookie2 != 0x89BACDFE) {
+  /*if (cookie1 != 0x2135467 || cookie2 != 0x89BACDFE) {
     abort('Stack overflow! Stack cookie has been overwritten, expected hex dwords 0x89BACDFE and 0x2135467, but received 0x' + cookie2.toString(16) + ' 0x' + cookie1.toString(16));
-  }
+  }*/
   // Also test the global address 0 for integrity.
   if (HEAP32[0] !== 0x63736d65 /* 'emsc' */) abort('Runtime error: The application has corrupted its heap memory area (address zero)!');
 }
