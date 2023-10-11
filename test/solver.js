0ff2ad0d44d38666c76ab07c24f9e02818218c3c
jgn <gorinje@gmail.com>
2023-10-11 13:34:12 +0200

    Add new openjpeg tests

--- test/solver.js
+++ test/solver.js
@@ -4,7 +4,7 @@ describe('solver', () => {
 		create_test('img',
 			'universal-img',
 			"solver_1",
-			"libjxl_1;libpng_1;libjpeg_1",
+			"libjxl_1;libpng_1;libjpeg_1;openjpeg_1",
 			"https://bevara.ddns.net/test-signals/JXL/test.jxl",
 			"https://bevara.ddns.net/test-signals/out/jxl/test.png",
 			done,
