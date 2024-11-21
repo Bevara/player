2a1641a363d799c2bdb50ea8794635bf6a8f5731
Jérôme Gorin <gorinje@gmail.com>
2024-11-21 10:42:49 +0000

    Fix wrong ip

--- push_to_docker.sh
+++ push_to_docker.sh
@@ -2,6 +2,7 @@ mkdir -p build/
 cd build
 ../configure
 make
+cd ..
 docker build -t player .
-docker tag  player 192.168.1.120:32000/player:1.0.0
-docker push  192.168.1.120:32000/player:1.0.0
+docker tag  player localhost:32000/player:1.0.0
+docker push localhost:32000/player:1.0.0
