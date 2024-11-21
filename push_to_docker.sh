4f8c6079051a71fb07253eadb710d989ffc94d02
Jérôme Gorin <gorinje@gmail.com>
2024-11-21 11:53:47 +0000

    Fix script

--- push_to_docker.sh
+++ push_to_docker.sh
@@ -1,8 +1,11 @@
+#!/bin/bash
+version="1.0.2"
+
 mkdir -p build/
 cd build
 ../configure
 make
 cd ..
 docker build -t player .
-docker tag  player localhost:32000/player:1.0.0
-docker push localhost:32000/player:1.0.0
+docker tag  player localhost:32000/player:$version
+docker push localhost:32000/player:$version
